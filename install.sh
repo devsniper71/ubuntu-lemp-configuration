92541aff1cc11f9cbddd5a5c3b1376e2116d9ed1
Md Rasel Ahmed <brimful.00.plight@icloud.com>
2022-09-24 02:19:31 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -68,7 +68,7 @@ apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-he
 php8.1-common \
 php8.1-mysql \
 php8.1-xml php8.1-xmlrpc \
-php8.1-gd php8.1-imagick \
+php8.1-curl php8.1-gd php8.1-imagick \
 php8.1-cli php8.1-dev php8.1-imap php8.1-mbstring \
 php8.1-opcache php8.1-soap php8.1-zip php8.1-redis php8.1-intl \
 php8.1-sqlite3 \
